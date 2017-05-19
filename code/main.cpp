bd8f5f9834973085d8141fc7d8824e62f2830414
Hang Yan <yanhangxiong@gmail.com>
2017-05-19 06:15:06 -0500

    Hang

--- code/main.cpp
+++ code/main.cpp
@@ -12,8 +12,8 @@ std::vector<cv::Vec3b> colors{cv::Vec3b(255, 0, 0), cv::Vec3b(0, 255, 0), cv::Ve
                               cv::Vec3b(255, 128, 0), cv::Vec3b(255, 0, 128), cv::Vec3b(0, 255, 128)};
 
 int main(int argc, char** argv){
-    if(argc < 2){
-        cerr << "Usage ./PlaneStereo <path-to-file>" << endl;
+    if(argc < 3){
+        cerr << "Usage ./PlaneStereo <path-to-file> <output-path>" << endl;
         return 1;
     }
     google::InitGoogleLogging(argv[0]);
@@ -33,26 +33,26 @@ int main(int argc, char** argv){
     DPM::GeneratePlanes(vertices, planes, plane_map, 10000, 0.5);
     printf("%d planes fitted", (int)planes.size());
 
-    cv::resize(plane_map, plane_map, cv::Size(500, 500), 0, 0, cv::INTER_NEAREST);
-    cv::imshow("plane_map", plane_map);
-    cv::waitKey(0);
+//    cv::resize(plane_map, plane_map, cv::Size(500, 500), 0, 0, cv::INTER_NEAREST);
+//    cv::imshow("plane_map", plane_map);
+//    cv::waitKey(0);
 
     std::vector<Eigen::Vector3d> new_vertices;
     std::vector<int> plane_assignment;
     printf("Solving...\n");
     DPM::SolvePlaneStereo(mesh.vertices, planes, new_vertices, plane_assignment, 5.0);
 
-    cv::Mat new_plane_map(W, W, CV_8UC3, cv::Scalar(255, 255, 255));
-    for(auto i=0; i<W*W; ++i){
-        new_plane_map.at<cv::Vec3b>(i/W, i%W) = colors[plane_assignment[i] % (int)colors.size()];
-    }
-    cv::resize(new_plane_map, new_plane_map, cv::Size(500, 500), 0, 0, cv::INTER_NEAREST);
-    cv::imshow("Plane assignment", new_plane_map);
-    cv::waitKey(0);
+//    cv::Mat new_plane_map(W, W, CV_8UC3, cv::Scalar(255, 255, 255));
+//    for(auto i=0; i<W*W; ++i){
+//        new_plane_map.at<cv::Vec3b>(i/W, i%W) = colors[plane_assignment[i] % (int)colors.size()];
+//    }
+//    cv::resize(new_plane_map, new_plane_map, cv::Size(500, 500), 0, 0, cv::INTER_NEAREST);
+//    cv::imshow("Plane assignment", new_plane_map);
+//    cv::waitKey(0);
 
     DPM::Mesh result_mesh;
     result_mesh.vertices = new_vertices;
-    result_mesh.Write("result.obj");
+    result_mesh.Write(argv[2]);
 
     return 0;
 }
